a3eca9b16fee380ad7abf10e78ac53a2f0c05d47
moyersd28-cyber <moyersd28@gmail.com>
2026-07-15 12:19:07 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,44 @@
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+// Replace with YOUR Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyA2OwmcAMpY-buYi0DvMO3ezHFgau8Lzdo",
+  authDomain: "dispatchone-30a6f.firebaseapp.com",
+  projectId: "dispatchone-30a6f",
+  storageBucket: "dispatchone-30a6f.firebasestorage.app",
+  messagingSenderId: "335571852717",
+  appId: "1:335571852717:web:5b6641a0b591a69ffa5524"
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+// Services
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+// Export
+
+export {
+
+    auth,
+
+    db
+
+};
