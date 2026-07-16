dfb99060a836616be061210064945124cf5e2e94
moyersd28-cyber <moyersd28@gmail.com>
2026-07-15 20:55:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE = "dispatchone-v1";
 
+const FILES = [
+  "/",
+  "/index.html",
+  "/css/main.css",
+  "/css/login.css"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
